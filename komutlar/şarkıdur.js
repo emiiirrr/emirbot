fdddab958c4782b6ecb18b87eb1bcf1403f81ce0
emiiirrr <76749792+emiiirrr@users.noreply.github.com>
2020-12-30 01:19:54 +0300

    Add files via upload

---
+++ komutlar/şarkıdur.js
@@ -0,0 +1,20 @@
+const { MessageEmbed } = require('discord.js')
+module.exports = {
+    kod: "dc",
+    async run (client, message, args) {
+        if (message.member.voice.channel) {
+            const dc = await message.member.voice.channel.leave()
+            const embed = new MessageEmbed()
+            .setColor("RED")
+            .setTitle('Bağlantı Kesildi 📭')
+            .setFooter(message.author.tag, message.author.displayAvatarURL({dynamic: true}))
+            message.channel.send(embed, dc)
+        } else {
+            message.reply("Sesli bir kanalda değilim! ❌").then(sil => {
+                setTimeout(() => {
+                    sil.delete()
+                }, 5000);
+            })
+        }
+    }
+}
