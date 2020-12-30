1d0f9b117a5e30eca7e365e7ca33dcb25f37e512
emiiirrr <76749792+emiiirrr@users.noreply.github.com>
2020-12-30 15:10:57 +0300

    Update ayıps.js

--- komutlar/ayıps.js
+++ komutlar/ayıps.js
@@ -6,7 +6,7 @@ module.exports = {
         if (member) {
             const embed = new MessageEmbed()
             .setImage('https://s3.us-east-1.amazonaws.com/storia-storage/staged/host-prod/yAVZKibRBmpnNwAUSyP8Cs01L0oMhbC9fn6Jf7Ot.jpeg')
-            message.channel.send(`**${message.author.username}** Tarafından s*kildin **${member.username}**`, embed)
+            message.channel.send(`**${message.author.username}** seni s*kti. **${member.username}**`, embed)
         } else {
             message.reply("Kullanıcıyı etiketlemedin! ❌").then(mesaj4 => {
                 setTimeout(function () {
