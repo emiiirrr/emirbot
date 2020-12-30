bae7a1df39e271687f48ae4fd6609ec3b5abfe71
emiiirrr <76749792+emiiirrr@users.noreply.github.com>
2020-12-30 15:07:12 +0300

    Add files via upload

---
+++ komutlar/ayıps.js
@@ -0,0 +1,18 @@
+const { MessageEmbed } = require('discord.js')
+module.exports = {
+    kod: "fuck",
+    async run (client, message, args) {
+        let member = message.mentions.users.first();
+        if (member) {
+            const embed = new MessageEmbed()
+            .setImage('https://s3.us-east-1.amazonaws.com/storia-storage/staged/host-prod/yAVZKibRBmpnNwAUSyP8Cs01L0oMhbC9fn6Jf7Ot.jpeg')
+            message.channel.send(`**${message.author.username}** Tarafından s*kildin **${member.username}**`, embed)
+        } else {
+            message.reply("Kullanıcıyı etiketlemedin! ❌").then(mesaj4 => {
+                setTimeout(function () {
+                  mesaj4.delete()
+                }, 3000);
+              });            
+        }
+    }
+}
