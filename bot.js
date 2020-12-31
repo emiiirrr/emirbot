5d33b850cbcf419ac5fb6b9bed50fc99f81b9902
emiiirrr <76749792+emiiirrr@users.noreply.github.com>
2020-12-31 11:42:38 +0300

    Update bot.js

--- bot.js
+++ bot.js
@@ -1,22 +1,42 @@
-const Discord = require('discord.js') // discord.js modülü tanımlıyoruz.
-const client = new Discord.Client() // client tanımalamsı
-const { readdirSync } = require('fs'); // tanımlamalar
-const { join } = require('path'); // tanımlamalar
+const Discord = require('discord.js')
+const client = new Discord.Client()
+const { readdirSync } = require('fs'); 
+const { join } = require('path'); 
 const { Client, MessageEmbed } = require('discord.js');
 const { TOKEN, PREFIX } = require("./config.json")
 
-client.commands= new Discord.Collection(); // komutları alıyoruz
+client.commands= new Discord.Collection();
 
 client.prefix = PREFIX
 
-const commandFiles = readdirSync(join(__dirname, "komutlar")).filter(file => file.endsWith(".js")); // Belli bir klasörden belli .js uzantılı dosyaları buluyor.
-
-for (const file of commandFiles) {
+const cmd = readdirSync(join(__dirname, "komutlar")).filter(file => file.endsWith(".js")); 
+for (const file of cmd) {
     const command = require(join(__dirname, "komutlar", `${file}`));
-    client.commands.set(command.kod, command); // Komutları Ayarlıyoruz.
+    client.commands.set(command.kod, command);
+    
 }
 
 client.on("error", console.error);
+
+client.on("message", async message => {
+  if(message.author.bot) return;
+
+  if(message.content.startsWith(PREFIX)) {
+      const args = message.content.slice(PREFIX.length).trim().split(/ +/);
+      const command = args.shift().toLowerCase();
+
+      if(!client.commands.has(command)) return message.channel.send(`Komut dosyamda **${command}** adlı bir komut bulamadım.`);
+      
+      if (!command) return;
+
+      try {
+          client.commands.get(command).run(client, message, args);
+
+      } catch (error){
+          console.error(error);
+      }
+  }
+});
 
 client.on('ready', () => {
   console.log(`Botun geldi kanka ${client.user.tag}!`)
@@ -27,63 +47,23 @@ client.on('ready', () => {
   ]
   setInterval(function () {
     let durum = durumlar[Math.floor(Math.random()*durumlar.length)]
-    client.user.setActivity(durum)
+    client.user.setActivity(durum, { type: "LISTENING "})
   }, 10000)
   client.user.setStatus('online')
 });
-client.on("message", async message => {
-
-    if(message.author.bot) return;
-
-    if(message.content.startsWith(PREFIX)) {
-        const args = message.content.slice(PREFIX.length).trim().split(/ +/);
-
-        const command = args.shift().toLowerCase();
-
-        if(!client.commands.has(command)) return message.channel.send(`Komut dosyamda **${command}** adlı bir komut bulamadım.`);
-
-
-        try {
-            client.commands.get(command).run(client, message, args);
-
-        } catch (error){
-            console.error(error);
-        }
-    }
-})
-
 
 //BURADAN BAŞLAR
 
 client.on('message', msg => {
-  if (msg.content.toLowerCase() === '!şadi') {
-    msg.channel.send('Balım');
-  }
-  if (msg.content.toLowerCase() === '!bug') {
-    msg.channel.send('!bug');
-  }
-  if (msg.content.toLowerCase() === '!emre') {
-    msg.channel.send('Kuzuummm');
-  }
   if (msg.content.toLowerCase() === 'adem out') {
     msg.delete(msg.author)
     const adem = new MessageEmbed()
     .setTitle('***Uçabildiğin kadar uç...***')
-    .setAuthor('Adem out')
+    .setAuthor('Adem Out')
     .setColor("RANDOM")
     .setImage('https://media.giphy.com/media/HpegoSJeLAMkyilldk/giphy.gif');
     msg.channel.send(adem);
   }
-  if (msg.content.toLowerCase() === 'üzdüm mü seni') {
-    msg.channel.send('Kalbim çıt, gözyaşım pıt');
-  }
-});
-
-
-client.on('guildBanAdd' , (guild, user) => {
-  let genel = guild.channels.find('name', 'genel');
-  if (!genel) return;
-  genel.send('' );
 });
 
 client.on('message', msg => {
@@ -91,7 +71,7 @@ client.on('message', msg => {
     msg.delete(msg.author)
     const kaan = new MessageEmbed()
     .setTitle('***Bravo six going dark...***')
-    .setAuthor('Kaaninho out')
+    .setAuthor('Kaaninho Out')
     .setColor("RANDOM")
     .setImage('https://media.giphy.com/media/jQGE7t0qcFYzr3EHsF/giphy.gif');
     msg.channel.send(kaan)
@@ -102,7 +82,7 @@ client.on('message', msg => {
   if (msg.content.toLowerCase() === 'ali out') {
     msg.delete(msg.author)
     const ali = new MessageEmbed()
-    .setAuthor('Ali out')
+    .setAuthor('Ali Out')
     .setColor("RANDOM")
     .setImage('https://media1.tenor.com/images/ccd44330b921bba21e897e708955e722/tenor.gif');
     msg.channel.send(ali)
@@ -113,9 +93,10 @@ client.on('message', message => {
   if (message.content.toLocaleLowerCase() === 'elif out') {
     message.delete(message.author)
     const elif = new MessageEmbed()
-    .setAuthor('Elif out')
+    .setTitle('I Love You 3000...')
+    .setAuthor('Elif Out')
     .setColor("RED")
-    .setImage('https://i.pinimg.com/originals/89/bb/3b/89bb3b3bb331cb3dde823c653d82d3d1.png')
+    .setImage('https://media1.tenor.com/images/300209b23f0e0470931912cdaf6924de/tenor.gif')
     message.channel.send(elif)
   }
 })
@@ -146,10 +127,101 @@ client.on('message', message => {
   if (message.content.toLocaleLowerCase() === 'ömer out') {
     message.delete(message.author)
     const ömer = new MessageEmbed()
+    .setAuthor('Ömer Out')
+    .setColor("RED")
+    .setImage('https://media1.tenor.com/images/a674181d59056ab132376921367eb266/tenor.gif')
+    message.channel.send(ömer)
+  }
+})
+
+client.on('message', message => {
+  if (message.content.toLocaleLowerCase() === 'yogurt out') {
+    message.delete(message.author)
+    const elifnaz = new MessageEmbed()
+    .setTitle('Bihter kaçar')
+    .setAuthor('Yogurt Out')
+    .setColor("RED")
+    .setImage('https://media.giphy.com/media/s7cP0gvnf4uy1RwCLg/giphy.gif')
+    message.channel.send(elifnaz)
+  }
+})
+
+client.on('message', message => {
+  if (message.content.toLocaleLowerCase() === 'tuna out') {
+    message.delete(message.author)
+    const tuna = new MessageEmbed()
+    .setTitle('Behlül kaçar')
+    .setAuthor('Tuna Out')
+    .setColor("RED")
+    .setImage('https://i.ibb.co/ZczBH2v/images-3.jpg')
+    message.channel.send(tuna)
+  }
+})
+
+client.on('message', message => {
+  if (message.content.toLocaleLowerCase() === 'nes out') {
+    message.delete(message.author)
+    const nes = new MessageEmbed()
+    .setAuthor('Enesquik Out')
+    .setColor("RED")
+    .setImage('https://media.giphy.com/media/V4efeXVTdKExy/giphy.gif')
+    message.channel.send(nes)
+  }
+})
+
+client.on('message', message => {
+  if (message.content.toLocaleLowerCase() === 'monzter out') {
+    message.delete(message.author)
+    const monzter = new MessageEmbed()
     .setAuthor('Söylemediniz ')
     .setColor("RED")
     .setImage('https://blog.hubspot.com/hubfs/Shrug-Emoji.jpg')
-    message.channel.send(ömer)
+    message.channel.send(monzter)
+  }
+})
+
+client.on('message', message => {
+  if (message.content.toLocaleLowerCase() === 'harun out') {
+    message.delete(message.author)
+    const harun = new MessageEmbed()
+    .setAuthor('Söylemediniz ')
+    .setColor("RED")
+    .setImage('https://blog.hubspot.com/hubfs/Shrug-Emoji.jpg')
+    message.channel.send(harun)
+  }
+})
+
+client.on('message', message => {
+  if (message.content.toLocaleLowerCase() === 'özge out') {
+    message.delete(message.author)
+    const özge = new MessageEmbed()
+    .setAuthor('Söylemediniz ')
+    .setColor("RED")
+    .setImage('https://blog.hubspot.com/hubfs/Shrug-Emoji.jpg')
+    message.channel.send(özge)
+  }
+})
+
+client.on('message', message => {
+  if (message.content.toLocaleLowerCase() === 'emre out') {
+    message.delete(message.author)
+    const emre = new MessageEmbed()
+    .setAuthor('Söylemediniz ')
+    .setColor("RED")
+    .setImage('https://blog.hubspot.com/hubfs/Shrug-Emoji.jpg')
+    message.channel.send(emre)
+  }
+})
+
+
+client.on('message', message => {
+  if (message.content.toLocaleLowerCase() === 'umut out') {
+    message.delete(message.author)
+    const umut = new MessageEmbed()
+    .setAuthor('Söylemediniz ')
+    .setColor("RED")
+    .setImage('https://blog.hubspot.com/hubfs/Shrug-Emoji.jpg')
+    message.channel.send(umut)
   }
 })
 
