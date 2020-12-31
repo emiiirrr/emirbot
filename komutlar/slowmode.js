8be5f5a67c82b2add6c830e75808755a3733ee82
emiiirrr <76749792+emiiirrr@users.noreply.github.com>
2020-12-31 20:59:14 +0300

    Update slowmode.js

--- komutlar/slowmode.js
+++ komutlar/slowmode.js
@@ -1,5 +1,6 @@
 module.exports = {
     kod: "slowmode" ,
+    aliases: ["sm"],
     async run (client, message, args) {
         const ms = require('rhino-ms')
         if (!message.member.hasPermission('MANAGE_CHANNELS')) return message.reply('Bunu yapamazsın! ❌').then(mesaj1 => {
